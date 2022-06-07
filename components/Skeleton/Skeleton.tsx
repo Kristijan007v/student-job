cfce97fe729d076c41c4a9fb76f9f00bc35122db
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-06-08 01:06:47 +0200

    Add new icons, improved sign in and register pages

--- components/Skeleton/Skeleton.tsx
+++ components/Skeleton/Skeleton.tsx
@@ -1,11 +1,70 @@
-import React from "react";
+import { AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
 import { Toaster } from "react-hot-toast";
+import MobileNavigation from "../MobileNavigation/MobileNavigation";
+import Overlay from "../Overlay/Overlay";
+import Search from "../Search/Search";
 
 interface Props {
   children: React.ReactNode;
+  mobileNavigation?: boolean;
 }
 
-export default function Skeleton({ children }: Props) {
+const items = {
+  data: [
+    {
+      section: {
+        id: "1",
+        title: "Settings",
+        content: [
+          {
+            id: "1",
+            title: "Font size",
+          },
+          {
+            id: "2",
+            title: "Dark mode",
+          },
+        ],
+      },
+    },
+    {
+      section: {
+        id: "2",
+        title: "Pages",
+        content: [
+          {
+            id: "1",
+            title: "Profile",
+          },
+          {
+            id: "2",
+            title: "Home",
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default function Skeleton({ children, mobileNavigation }: Props) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [results, setResults] = useState(items);
+
+  const [searchOpen, setSearchOpen] = useState(false);
+
+  const searchItems = (term: string) => {
+    console.log(term);
+    //Filter items content.title based on search term
+    const filteredItems = items.data.filter((item) => {
+         
+
+    console.log(filteredItems);
+    setResults(filteredItems);
+  }
+
+
   return (
     <div>
       {/* Toast notifications */}
@@ -13,8 +72,67 @@ export default function Skeleton({ children }: Props) {
         <Toaster />
       </div>
 
+      {/* Mobile navigation */}
+      {mobileNavigation && (
+        <MobileNavigation
+          openSearch={() => {
+            setSearchOpen(true);
+          }}
+        />
+      )}
+
       {/* Main content goes here */}
       <div>{children}</div>
+
+      {/* Footer goes here */}
+
+      {/* Application search */}
+      <AnimatePresence exitBeforeEnter>
+        {searchOpen && (
+          <Overlay
+            closeOverlay={() => {
+              setSearchOpen(false);
+            }}
+            contentPosition={"top"}
+            blur
+          >
+            <>
+              <div>
+                <Search onchange={(e) => searchItems(e.target.value)} />
+              </div>
+              {/* <div className="mt-4 flex flex-col space-y-1.5">
+                {results.data.length > 0 ? (
+                  <>
+                    {results.data.map((item) => (
+                      <>
+                        <h3
+                          key={item.section.id}
+                          className="font-medium text-black"
+                        >
+                          {item.section.title}
+                        </h3>
+                        {item.section.items.map((subItem) => (
+                          <div
+                            key={subItem.id}
+                            className="rounded-md bg-white p-4"
+                          >
+                            {subItem.title}
+                          </div>
+                        ))}
+                      </>
+                    ))}
+                  </>
+                ) : (
+                  <div className="text-center">
+                    <h3 className="font-medium text-black">No results</h3>
+                  </div>
+                )}
+              </div> */}
+            </>
+          </Overlay>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
+
